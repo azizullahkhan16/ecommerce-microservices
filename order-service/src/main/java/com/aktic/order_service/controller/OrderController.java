2ca40c4a1bd098f2148319ed232c2439f798ac21
Azizullah Khan <aziz.bin.aman16@gmail.com>
2024-09-26 17:00:29 +0500

    Added circuit breaker

--- order-service/src/main/java/com/aktic/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/aktic/order_service/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.aktic.order_service.controller;
 import com.aktic.order_service.dto.OrderRequest;
 import com.aktic.order_service.dto.OrderResponse;
 import com.aktic.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +20,13 @@ public class OrderController {
 
 
     @PostMapping
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
     public ResponseEntity<OrderResponse> placeOrder (@RequestBody OrderRequest orderRequest) {
         return orderService.placeOrder(orderRequest);
+    }
+
+    public ResponseEntity<OrderResponse> fallBackMethod (OrderRequest orderRequest, RuntimeException runtimeException) {
+        throw new IllegalArgumentException("Oops! something went wrong");
     }
 
 
