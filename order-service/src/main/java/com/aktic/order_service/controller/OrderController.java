d15db0ab2d95628b97bb631202529d31f4d19d7e
Azizullah Khan <aziz.bin.aman16@gmail.com>
2024-09-27 21:11:09 +0500

    adde circuit breaker implementation

--- order-service/src/main/java/com/aktic/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/aktic/order_service/controller/OrderController.java
@@ -4,12 +4,17 @@ import com.aktic.order_service.dto.OrderRequest;
 import com.aktic.order_service.dto.OrderResponse;
 import com.aktic.order_service.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import org.apache.hc.core5.concurrent.CompletedFuture;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -21,11 +26,13 @@ public class OrderController {
 
     @PostMapping
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
-    public ResponseEntity<OrderResponse> placeOrder (@RequestBody OrderRequest orderRequest) {
-        return orderService.placeOrder(orderRequest);
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<ResponseEntity<OrderResponse>> placeOrder (@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
 
-    public ResponseEntity<OrderResponse> fallBackMethod (OrderRequest orderRequest, RuntimeException runtimeException) {
+    public CompletableFuture<ResponseEntity<OrderResponse>> fallBackMethod (OrderRequest orderRequest, RuntimeException runtimeException) {
         throw new IllegalArgumentException("Oops! something went wrong");
     }
 
